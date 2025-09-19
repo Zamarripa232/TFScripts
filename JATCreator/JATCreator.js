c6aec3bcbbbe538795acfca0c10023243afa1780
Floyd Zamarripa <Zamarripa232@gmail.com>
2025-09-19 16:05:12 -0400

    Initially functional JATCreator script completed and executed successfully.

--- JATCreator/JATCreator.js
+++ JATCreator/JATCreator.js
@@ -4,11 +4,38 @@ function createJATs() {
   const namesTab = referenceSheet.getSheetByName('StudentList');
   const namesData = namesTab.getDataRange().getValues();
 
-  // TODO Get the base Spreadsheet to replicate
-  // TODO Set the target directory to have new sheets moved to
-  // TODO Get the names of the tabs to work with
-  // TODO Loop of reading the sheet row by row
-    // TODO within loop, create copy the base sheet with name First Last JAT
-    // TODO add the associated email to the shared list for that file only
-    // TODO move the file if necessary to the target directory
+  // Get the base Spreadsheet to replicate
+  // The Value in A1 of the spreadsheet is the document ID (the part after the d in a google drive link)
+  // of the blank document to copy.
+  const sourceSheet = referenceSheet.getSheetByName('SourceFile');
+  const sourceFileID = sourceSheet.getRange('A1').getValue().trim(); // .trim() just in case I added spaces or a newline by accident in the future
+  const sourceFile = DriveApp.getFileById(sourceFileID);
+
+  // Set the target directory to have new sheets moved to.
+  // This is A2 in the JATCreator's SourceFile tab
+  const targetFolderID = sourceSheet.getRange('A2').getValue().trim(); 
+  const targetFolder = DriveApp.getFolderById(targetFolderID);
+
+  // Loop of reading the sheet row by row, i starts at 1 so that it skips the first row (headers)
+  for (let i = 1; i < namesData.length; i++) {
+    const firstName = namesData[i][0];
+    const lastName = namesData[i][1];
+    const email = namesData[i][2];
+
+    // Create copy the base sheet with name structure: First Last JAT
+    // turns out makeCopy(name, destination ID) can also have it created
+    // in the right spot
+    const newFileName = firstName + ' ' + lastName + ' JAT';
+    const newFile = sourceFile.makeCopy(newFileName, targetFolder);
+
+    // add the associated email to the shared list for that file only
+    newFile.addEditor(email)
+
+    // Add the newly created file's URL back to the URL column of the JATCreator sheet
+    const url = 'https://docs.google.com/spreadsheets/d/' + newFile.getId() + '/';
+    namesTab.getRange(i+1, 4).setValue(url);
+
+    // Before looping again, wait 0.2 seconds in case of rate limits
+    Utilities.sleep(200);
+  }
 }
